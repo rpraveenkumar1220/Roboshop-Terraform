1e64f10abd65fbc15533adedaf844092d4a2949b
rpraveenkumar1220 <rpraveenkumar1220@gmail.com>
2023-10-13 21:02:00 +0530

    Roboshop with terraform setup

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ output "test" {
 
 module "subnet"{
   source = "git::https://github.com/rpraveenkumar1220/Subnets-Module-Terraform.git"
-  for_each = var.subnets
+  for_each = var.vpc["subnets"]
   cidr_block = each.value["cidr_block"]
   vpc_id = data.aws_vpc.selected.id
   env = var.env
