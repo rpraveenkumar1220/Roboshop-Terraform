3c07260e4f8f55768664534f68ef7af2ec201d96
rpraveenkumar1220 <rpraveenkumar1220@gmail.com>
2023-10-14 12:42:03 +0530

    Roboshop with terraform setup

--- main.tf
+++ main.tf
@@ -7,12 +7,15 @@ module "app"{
 }
 */
 
+
  module "vpc" {
+
   source = "git::https://github.com/rpraveenkumar1220/VPC-Module-Terraform.git"
   for_each = var.vpc
   env = var.env
   cidr_block = each.value["cidr_block"]
   subnets = each.value["subnets"]
+
   }
 
 
