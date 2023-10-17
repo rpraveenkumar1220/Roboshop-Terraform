bba7757ecd30c00bd391a5c0777db657b3535a4b
rpraveenkumar1220 <rpraveenkumar1220@gmail.com>
2023-10-17 11:06:44 +0530

    Roboshop with terraform setup

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ module "app_instance" {
  source = "git::https://github.com/rpraveenkumar1220/App-Module-Terraform.git"
  env = var.env
  component = "test"
- subnet_id = lookup(lookup(lookup(var.vpc,"main",null ),"app",null),"cidr_block",null)))[0]
+ subnet_id = element(lookup(lookup(lookup(var.vpc,"main",null ),"app",null),"cidr_block",null))))[0]
 }
 
 
