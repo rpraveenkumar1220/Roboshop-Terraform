6795edd4eb060d6227eb8ef0cb99a19d2407d281
rpraveenkumar1220 <rpraveenkumar1220@gmail.com>
2023-11-05 12:33:21 +0530

    Roboshop with terraform setup

--- main.tf
+++ main.tf
@@ -38,7 +38,7 @@ module "rabbitmq" {
  subnet_id     = lookup(lookup(lookup(lookup(module.vpc, "main", null ), "subnet_ids", null), "db", null), "subnet_ids", null)[0]
  sg_subnet_cidr = lookup(lookup(lookup(lookup(module.vpc,"main",null ),"subnet_ids",null),"app",null),"cidr_block",null)
  vpc_id        = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
-
+ kms_key_id  = var.kms_key_id
  env            = var.env
  allow_ssh_cidr = var.allow_ssh_cidr
 }
