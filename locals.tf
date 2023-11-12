9b62108e8a622e280d0316af3d2573505d14ba37
rpraveenkumar1220 <rpraveenkumar1220@gmail.com>
2023-11-12 11:54:53 +0530

    Creating RDS,DOCDB and Elasticache Services

--- locals.tf
+++ locals.tf
@@ -1,7 +1,8 @@
 locals {
 
-  public_web_subnets_cidr = concat(lookup(lookup(lookup(lookup(var.vpc, "main", null ), "subnets", null), "public", null), "cidr_block", null), lookup(lookup(lookup(lookup(module.vpc, "main", null ), "subnet_ids", null), "web", null), "cidr_block", null))
+  public_web_subnets_cidr = concat(lookup(lookup(lookup(lookup(var.vpc, "main", null ), "subnets", null), "public", null), "cidr_block", null), lookup(lookup(lookup(lookup(var.vpc, "main", null ), "subnet_ids", null), "web", null), "cidr_block", null))
 
-  web_app_subnets_cidr = concat(lookup(lookup(lookup(lookup(var.vpc, "main", null ), "subnets", null), "web", null), "cidr_block", null), lookup(lookup(lookup(lookup(module.vpc, "main", null ), "subnet_ids", null), "app", null), "cidr_block", null))
+  web_app_subnets_cidr = concat(lookup(lookup(lookup(lookup(var.vpc, "main", null ), "subnets", null), "web", null), "cidr_block", null), lookup(lookup(lookup(lookup(var.vpc, "main", null ), "subnet_ids", null), "app", null), "cidr_block", null))
+
 
 }
