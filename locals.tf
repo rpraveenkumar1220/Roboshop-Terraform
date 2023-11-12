ca41cff0c5e0bf8ac757d9e26c9b647be62ec799
rpraveenkumar1220 <rpraveenkumar1220@gmail.com>
2023-11-12 12:00:32 +0530

    Creating APP modules

--- locals.tf
+++ locals.tf
@@ -1,8 +1,6 @@
 locals {
 
-  public_web_subnets_cidr = concat(lookup(lookup(lookup(lookup(var.vpc, "main", null ), "subnets", null), "public", null), "cidr_block", null), lookup(lookup(lookup(lookup(var.vpc, "main", null ), "subnet_ids", null), "web", null), "cidr_block", null))
+  public_web_subnets_cidr = concat(lookup(lookup(lookup(lookup(var.vpc, "main", null ), "subnets", null), "web", null), "cidr_block", null), lookup(lookup(lookup(lookup(var.vpc, "main", null ), "subnets", null), "public", null), "cidr_block", null))
 
-  web_app_subnets_cidr = concat(lookup(lookup(lookup(lookup(var.vpc, "main", null ), "subnets", null), "web", null), "cidr_block", null), lookup(lookup(lookup(lookup(var.vpc, "main", null ), "subnet_ids", null), "app", null), "cidr_block", null))
-
-
+  web_app_subnets_cidr = concat(lookup(lookup(lookup(lookup(var.vpc, "main", null ), "subnets", null), "web", null), "cidr_block", null), lookup(lookup(lookup(lookup(var.vpc, "main", null ), "subnets", null), "app", null), "cidr_block", null))
 }
