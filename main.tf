266d0a3e89f1ca35e1f3bc71a5d3669fc90b221f
rpraveenkumar1220 <rpraveenkumar1220@gmail.com>
2023-11-12 10:31:12 +0530

    Creating RDS,DOCDB and Elasticache Services

--- main.tf
+++ main.tf
@@ -117,14 +117,13 @@ module "apps" {
  desired_capacity = each.value["desired_capacity"]
  instance_type    = each.value["instance_type"]
  app_port         = each.value["instance_type"]
+ vpc_id           = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+ env              = var.env
 
+ subnets        = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), each.value["subnet_ref"], null), "subnet_ids", null)
+ listener_arn   = lookup(lookup(lookup(module.alb, "main", null), each.value["lb_ref"], null), "listener_arn", null)
  lb_dns_name    = lookup(lookup(lookup(module.alb, "main", null), each.value["lb_ref"], null), "dns_name", null)
  sg_subnet_cidr = each.value["component"] == "frontend" ? local.public_web_subnets_cidr : lookup(lookup(lookup(lookup(lookup(var.vpc, "main", null ), "subnets", null), each.value["subnet_ref"], null), "cidr_block", null)
-  subnets = lookup(lookup(lookup(lookup(module.vpc, "main", null),"subnet_ids",null),each.value["subnet_ref"],null),"subnet_ids",null)
-  listener_arn = lookup(lookup(lookup(module.alb, "main", null), each.value["lb_ref"], null), "listener_arn", null)
-
-  vpc_id = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
-  env    = var.env
   }
 
 
