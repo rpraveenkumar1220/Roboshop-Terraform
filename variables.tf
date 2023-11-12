3f0fc95dc9086527bc66f2d3a163375db702f4ea
rpraveenkumar1220 <rpraveenkumar1220@gmail.com>
2023-11-12 11:14:05 +0530

    Creating RDS,DOCDB and Elasticache Services

--- variables.tf
+++ variables.tf
@@ -6,20 +6,12 @@ variable "allow_ssh_cidr" {}
 variable "default_vpc_rt" {}
 variable "rds" {}
 variable "kms_key_arn" {}
+variable "kms_key_id" {}
 variable "docdb" {}
 variable "elasticache" {}
 variable "alb" {}
 variable "apps" {}
-variable "desired_capacity" {}
-variable "instance_type" {}
-variable "app_port" {}
-variable "lb_dns_name" {}
-variable "subnets" {}
-variable "min_size" {}
-variable "max_size" {}
-variable "subnet_ref" {}
-variable "lb_rule_priority" {}
-variable "lb_ref" {}
+
 
 
 
